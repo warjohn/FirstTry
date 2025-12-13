5c3e068376448beb6f6fed20a2d09bf711d50d08
warjohn <johnvoronina@gmail.com>
2025-12-13 19:09:49 +0300

    fix_misstakes

--- mlops/app/__init__.py
+++ mlops/app/__init__.py
@@ -3,15 +3,16 @@ from .logger_config import configure_logger
 from .models import SalaryPredictor
 from .routes import init_routes
 
-def create_app(modelPath, preprocessPath):
+def create_app(modelPath = None, preprocessPath = None):
     app = Flask(__name__)
     predictor = SalaryPredictor()
     
-    try:
-        predictor.load_model(modelPath, preprocessPath)
-        app.logger.info("Model and preprocessor loaded successfully")
-    except Exception as e:
-        app.logger.error(f"Error loading model: {str(e)}")
+    if modelPath and preprocessPath:
+        try:
+            predictor.load_model(modelPath, preprocessPath)
+            app.logger.info("Model and preprocessor loaded successfully")
+        except Exception as e:
+            app.logger.error(f"Error loading model: {str(e)}")
 
     init_routes(app, predictor)
     configure_logger(app)
