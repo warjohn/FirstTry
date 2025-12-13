fcc3ce018a651163809fb048c37b2ee1572f28e6
warjohn <johnvoronina@gmail.com>
2025-12-13 19:04:23 +0300

    fix_misstakes

--- mlops/app/__init__.py
+++ mlops/app/__init__.py
@@ -3,9 +3,16 @@ from .logger_config import configure_logger
 from .models import SalaryPredictor
 from .routes import init_routes
 
-def create_app():
+def create_app(modelPath, preprocessPath):
     app = Flask(__name__)
     predictor = SalaryPredictor()
+    
+    try:
+        predictor.load_model(modelPath, preprocessPath)
+        app.logger.info("Model and preprocessor loaded successfully")
+    except Exception as e:
+        app.logger.error(f"Error loading model: {str(e)}")
+
     init_routes(app, predictor)
     configure_logger(app)
     return app
--- mlops/run.py
+++ mlops/run.py
@@ -2,20 +2,13 @@ from app import create_app
 from app.models import predictor
 import os
 
-app = create_app()
 
 model_path = os.getenv('MODEL_PATH', 'data/model.pkl')
 preprocessor_path = os.getenv('PREPROCESSOR_PATH', 'data/preprocessor.pkl')
 PORT = os.getenv('PORT', 8000)
 
-try:
-    predictor.load_model(model_path, preprocessor_path)
-    app.logger.info("Model and preprocessor loaded successfully")
-except Exception as e:
-    app.logger.error(f"Error loading model: {str(e)}")
+app = create_app(model_path, preprocessor_path)
 
-from app.routes import init_routes
-init_routes(app, predictor)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT, debug=True)
