dbb1308973343af09c0d9327d32d04b52d8fc610
warjohn <johnvoronina@gmail.com>
2025-12-14 08:39:38 +0300

    Фикс ошибок распределние этапов сборки

--- scripts/ci/saveAndPost.sh
+++ scripts/ci/saveAndPost.sh
@@ -6,8 +6,13 @@ ARCHIVE_NAME="$2"
 USER="$3"
 SERVER_IP="$4"
 
-echo "Save docker image -> name : $ARCHIVE_NAME"
-docker save "$IMAGE_TAG" | gzip > /tmp/"$ARCHIVE_NAME".tar.gz
+if [[ -z "$IMAGE_TAG" || -z "$ARCHIVE_NAME" || -z "$USER" || -z "$SERVER_IP" ]]; then
+  echo "ERROR: One of the required arguments is empty after trimming."
+  exit 1
+fi
+
+echo "Save docker image -> name: [$IMAGE_TAG] -> archive: [$ARCHIVE_NAME]"
+docker save "$IMAGE_TAG" | gzip > "/tmp/${ARCHIVE_NAME}.tar.gz"
 
 echo "Send docker image to server"
 scp -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no \
