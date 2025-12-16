6bf4184943da6c54bde43059a38222dddf09c194
warjohn <johnvoronina@gmail.com>
2025-12-16 12:15:29 +0700

    добавления cloud_init в terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,31 +4,28 @@ data "twc_configurator" "premium_ru1" {
   preset_type   = "premium"
 }
 
-data "twc_os" "ubuntu_22" {
-  name    = "ubuntu"
-  version = "22.04"
-}
-
 resource "twc_ssh_key" "diploma_key" {
   name = "diploma-key"
   body = file(var.ssh_public_key_path)
 }
 
-data "twc_software" "docker_on_ubuntu" {
-  name = "Docker"
 
-  os {
-    name    = "ubuntu"
-    version = "22.04"
-  }
+data "twc_os" "os" {
+  name = "ubuntu"
+  version = "22.04"
+}
+
+data "twc_projects" "diplom" {
+  name = "diplom"
 }
 
 resource "twc_server" "diplom" {
   name         = "diplom"
-  os_id        = data.twc_os.ubuntu_22.id
-  software_id  = data.twc_software.docker_on_ubuntu.id
-  
+  os_id = data.twc_os.os.id
+
   ssh_keys_ids = [twc_ssh_key.diploma_key.id]
+
+  project_id = data.twc_projects.diplom.id
 
   configuration {
     configurator_id = data.twc_configurator.premium_ru1.id
@@ -36,9 +33,20 @@ resource "twc_server" "diplom" {
     cpu  = 2
     ram  = 2048     
   }
-
+  cloud_init = file("../scripts/server/dependencies.sh")
 }
 
-# output "server_ip" {
-#   value = twc_server.diploma_vm.networks[0].ip
-# }
+resource "twc_server_ip" "diplom" {
+  source_server_id = twc_server.diplom.id
+
+  type="ipv4"
+  ptr = "diplom.com"
+
+}
+output "server_public_ip" {
+  value = twc_server_ip.diplom.ip
+}
+
+output "server_public_ipv6" {
+  value = twc_server.diplom.networks[0].ips[0].ip
+}
